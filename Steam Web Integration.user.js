35633bad05c3f2ede1c7b34fbb1196675fd98119
Luckz <224748+Luckz@users.noreply.github.com>
2021-03-17 23:05:24 +0100

    V1.11.1

--- Steam Web Integration.user.js
+++ Steam Web Integration.user.js
@@ -29,7 +29,7 @@
 // @run-at       document-start
 // @supportURL   https://github.com/Revadike/SteamWebIntegration/issues/
 // @updateURL    https://github.com/Revadike/SteamWebIntegration/raw/master/Steam%20Web%20Integration.user.js
-// @version      1.11.0
+// @version      1.11.1
 // ==/UserScript==
 
 // ==Code==
